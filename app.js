149dd9d524fa2dd80c9195e27d49f1c36ef11045
Mel DeJesus <meldejesus22@gmail.com>
2019-01-09 16:41:29 -0500

    fixed /

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/bucket.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/current", function(req, res) {
